0b6d36563f43ab56fb1a4cb19f168f8f9a1c4193
Brynna McCarter and Keith Evans <brynna@>
2017-01-25 09:25:22 -0800

    add basic front-end interface and meet spec 3

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,23 @@ var currentScore = 0;
 
 function rollDie() {
   var roll =  Math.floor(Math.random() * 6) + 1;
-  currentScore += roll;
+  if (roll === 1) {
+    currentScore = 0;
+  } else {
+    currentScore += roll;
+  }
   return roll;
-
 }
 
 
 /////////////////////
 //Front-End
 /////////////////////
+
+$(function() {
+  $("#roll-button").click(function() {
+    var roll = rollDie();
+    $("#dice-roll").text(roll);
+    $("#current-score").text(currentScore);
+  });
+});
